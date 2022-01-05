d4d7f0f31e44c7a940974b498e80771090666129
Scott Reoch <scottreoch10@hotmail.co.uk>
2022-01-05 16:39:35 +0000

    5 tests passed

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -44,5 +44,33 @@ describe('Calculator', () => {
     expect(runningTotal.text()).toEqual('3')
   })
 
+  it('should multiply 3 by 5 to get 15', () => {
+    const button3 = container.find('#number3');
+    const buttonMultiply = container.find('#operator-multiply');
+    const button5 = container.find('#number5');
+    const buttonEquals = container.find('#operator-equals')
+    const runningTotal = container.find('#running-total');
+    button3.simulate('click');
+    buttonMultiply.simulate('click');
+    button5.simulate('click');
+    buttonEquals.simulate('click');
+    expect(runningTotal.text()).toEqual('15')
+  })
+
+  it('should divide 21 by 7 to get 3', () => {
+    const button2 = container.find('#number2');
+    const button1 = container.find('#number1');
+    const buttonDivide = container.find('#operator-divide');
+    const button7 = container.find('#number7');
+    const buttonEquals = container.find('#operator-equals');
+    const runningTotal = container.find('#running-total');
+    button2.simulate('click');
+    button1.simulate('click');
+    buttonDivide.simulate('click');
+    button7.simulate('click');
+    buttonEquals.simulate('click');
+    expect(runningTotal.text()).toEqual('3')
+  })
+
 })
 
